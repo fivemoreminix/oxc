d3e6f13a62669dc86349246640bdea45e7efbf8c
Luke Wilson <13669835+asmoaesl@users.noreply.github.com>
2018-08-19 19:11:37 -0500

    Code generation update for the new local variables.

--- src/generator.rs
+++ src/generator.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
 use ast::*;
 use scanner::*;
 
-fn generate_expression(expression: &Expr) -> String {
+type VariableMap = HashMap<String, isize>;
+
+fn generate_expression(expression: &Expr, variables: &mut VariableMap, stack_index: &mut isize) -> String {
     match expression {
         Expr::Const(num) => return format!("  movl ${}, %eax\n", num),
         Expr::BinOp(op, lhs, rhs) => {
@@ -11,15 +15,15 @@ fn generate_expression(expression: &Expr) -> String {
                     // We reverse who is in ecx register because subtraction is dst - src -> dst.
                     // Otherwise we'd have to `movl %ecx, %eax`. This is an optimization.
                     if *op == Operator::Minus || *op == Operator::Slash {
-                        generated = generate_expression(rhs);
+                        generated = generate_expression(rhs, variables, stack_index);
                         generated.push_str("  push %eax\n");
-                        generated.push_str(&generate_expression(lhs));
+                        generated.push_str(&generate_expression(lhs, variables, stack_index));
                         generated.push_str("  pop %ecx\n");
                         // rhs is now in ecx register
                     } else {
-                        generated = generate_expression(lhs);
+                        generated = generate_expression(lhs, variables, stack_index);
                         generated.push_str("  push %eax\n");
-                        generated.push_str(&generate_expression(rhs));
+                        generated.push_str(&generate_expression(rhs, variables, stack_index));
                         generated.push_str("  pop %ecx\n");
                         // lhs is now in ecx register
                     }
@@ -36,9 +40,9 @@ fn generate_expression(expression: &Expr) -> String {
                 Operator::EqualEqual  | Operator::NotEqual  | // Equality and comparison
                 Operator::LessThan    | Operator::LessEqual |
                 Operator::GreaterThan | Operator::GreaterEqual => {
-                    generated = generate_expression(rhs);
+                    generated = generate_expression(rhs, variables, stack_index);
                     generated.push_str("  push %eax\n");
-                    generated.push_str(&generate_expression(lhs)); // lhs is now in eax register
+                    generated.push_str(&generate_expression(lhs, variables, stack_index)); // lhs is now in eax register
                     generated.push_str("  pop %ecx\n"); // rhs is now in ecx register
                     generated.push_str("  cmpl %eax, %ecx\n  movl $0, %eax\n");
                     generated.push_str(match op {
@@ -52,9 +56,9 @@ fn generate_expression(expression: &Expr) -> String {
                     });
                 }
                 Operator::Or | Operator::And => {
-                    generated = generate_expression(lhs);
+                    generated = generate_expression(lhs, variables, stack_index);
                     generated.push_str("  push %eax\n");
-                    generated.push_str(&generate_expression(rhs));
+                    generated.push_str(&generate_expression(rhs, variables, stack_index));
                     generated.push_str("  pop %ecx\n");
                     generated.push_str(match op {
                         Operator::Or  => "  orl %ecx, %eax\n  movl $0, %eax\n  setne %al\n",
@@ -67,9 +71,9 @@ fn generate_expression(expression: &Expr) -> String {
                     });
                 }
                 _ if op.is_bitwise() => {
-                    generated = generate_expression(lhs);
+                    generated = generate_expression(lhs, variables, stack_index);
                     generated.push_str("  push %eax\n");
-                    generated.push_str(&generate_expression(rhs));
+                    generated.push_str(&generate_expression(rhs, variables, stack_index));
                     generated.push_str("  pop %ebx\n");
                     match op {
                         Operator::BitwiseAND => generated.push_str("  and %ebx, %eax\n"),
@@ -85,7 +89,7 @@ fn generate_expression(expression: &Expr) -> String {
             return generated;
         }
         Expr::UnaryOp(op, expr) => {
-            let mut generated_expr = generate_expression(expr);
+            let mut generated_expr = generate_expression(expr, variables, stack_index);
             match op {
                 Operator::LogicalNegation => {
                     generated_expr.push_str("  cmpl $0, %eax\n  sete %al\n");
@@ -98,21 +102,54 @@ fn generate_expression(expression: &Expr) -> String {
                 }
                 _ => unimplemented!(),
             }
-            return generated_expr
+            return generated_expr;
         }
-        _ => unimplemented!()
+        Expr::Assign(name, expr) => {
+            let mut output = generate_expression(expr, variables, stack_index);
+
+            if !variables.contains_key(name) {
+                panic!("Attempting to assign to an undeclared variable");
+            }
+
+            let offset: isize = *variables.get(name).unwrap();
+            output.push_str(&format!("  movl %eax, {}(%ebp)\n", offset));
+
+            return output;
+        }
+        Expr::Var(name) => {
+            if !variables.contains_key(name) {
+                panic!("Attempting to reference an undeclared variable");
+            }
+
+            let offset: isize = *variables.get(name).unwrap();
+            return format!("  movl {}(%ebp), %eax\n", offset);
+        }
     }
 }
 
-fn generate_statement(statement: &Statement) -> String {
+fn generate_statement(statement: &Statement, variables: &mut VariableMap, stack_index: &mut isize) -> String {
     let mut output = String::new();
     match statement {
         Statement::Return(expr) => {
-            let mut generated_expr = generate_expression(expr);
-            output.push_str(&generated_expr);
+            output.push_str(&generate_expression(expr, variables, stack_index));
             output.push_str("  ret\n");
         }
-        _ => unimplemented!(),
+        Statement::Declare(name, value) => {
+            if variables.contains_key(name) {
+                panic!("Can't declare a variable twice in the same scope");
+            }
+
+            if let Some(expr) = value {
+                output.push_str(&generate_expression(expr, variables, stack_index));
+                output.push_str("  pushl %eax\n");
+            } else {
+                output.push_str("  pushl $0\n");
+            }
+
+            variables.insert(name.clone(), *stack_index);
+            *stack_index -= 4;
+        }
+        Statement::Expr(expr) => output.push_str(&generate_expression(expr, variables, stack_index)),
     }
     output
 }
@@ -121,9 +158,18 @@ pub fn generate(ast: &Program) -> String {
     let mut output = String::new();
     match ast {
         Program::Func(name, statements) => { // Code generated when a function is made
+            let mut variable_map = VariableMap::new();
+            let mut stack_index = -4isize; // ESP - 4
             output.push_str(&format!("  .globl _{0}\n_{0}:\n", name));
+
             for statement in statements {
-                output.push_str(&generate_statement(statement));
+                output.push_str(&generate_statement(statement, &mut variable_map, &mut stack_index));
+            }
+
+            if !output.ends_with("ret\n") {
+                // No return issued, so we return zero by default
+                output.push_str("  movl $0, %eax\n");
+                output.push_str("  ret\n");
             }
         }
     }
